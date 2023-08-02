7077452ce0daa4826e1ccd7ac3c008b9a12f4511
Brian Kennedy <brian.kennedy@integrityxd.com>
2023-08-02 15:29:24 -0500

    Bundle files with webpack to include dependencies for npx

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './generators/app/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  externalsPresets: {
+    node: true
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
